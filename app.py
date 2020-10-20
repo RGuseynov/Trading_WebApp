4ab24b3cde398f67021cdab939e97c606e01a11f
Rustam GUSEYNOV <rustam.guseynov@exakis-nelite.com>
2020-10-20 17:06:21 +0200

    table html

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    table = [[1,2,3],[4,5,6],[7,8,9]]
+    return render_template('index.html', data=table)
+
