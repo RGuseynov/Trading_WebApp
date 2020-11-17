8ec61e9be1862700d89d65663a0b7a04ffb5db96
Rustam GUSEYNOV <rustam.guseynov@exakis-nelite.com>
2020-11-17 12:30:24 +0100

    round des proba et changement layout css

--- app.py
+++ app.py
@@ -13,24 +13,26 @@ app = Flask(__name__)
 def home():
     
     credential = DefaultAzureCredential()
-
     secret_client = SecretClient(vault_url="https://tradingkeyvault.vault.azure.net/", credential=credential)
     secret = secret_client.get_secret("sqlserver-trading")
-
 
     cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:sqlserver-trading.database.windows.net,1433;Database=financial;Uid=MasterTrader;Pwd="+secret.value+";Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
     cursor = cnxn.cursor()
 
-    cursor.execute("SELECT TOP(50) * FROM XGBoost_BSH_1H ORDER BY DatePrediction DESC") 
+    cursor.execute("SELECT TOP(200) * FROM XGBoost_BSH_1H ORDER BY DatePrediction DESC") 
     # row = cursor.fetchone()
     rows = cursor.fetchall()
     rows.reverse()
 
     new_rows =[]
     for row in rows:
-        row_list = [elem for elem in row]
+        # row_list = [elem for elem in row]
+        # row_list.append(max(row_list[1:]))
+
+        row_list = [row[0]]
+        row_list_rounded = list(map(lambda x: round(x, ndigits=4), row[1:]))
+        row_list.extend(row_list_rounded)
         row_list.append(max(row_list[1:]))
-        print(row_list)
         new_rows.append(row_list)
 
     return render_template('index.html', data=new_rows)
